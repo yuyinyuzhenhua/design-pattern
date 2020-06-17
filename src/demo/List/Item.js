78651147e00914970bde7ca5c03b86601c0720a6
yuzhenhua <yuzhenhua@bjhike.com>
2020-06-17 15:10:32 +0800

    a

--- src/demo/List/Item.js
+++ src/demo/List/Item.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 import getCart from '../ShoppingCart/GetCart'
 import StateMachine from 'javascript-state-machine'
+import { log } from '../util/log'
 
 export default class Item {
     constructor(list, data){
@@ -65,9 +66,12 @@ export default class Item {
         })
         $el.append($btn)
     }
+
+    @log('add')
     addToCartHandle(){
         this.cart.add(this.data)
     }
+    @log('del')
     deleteFromCartHandle(){
         this.cart.del(this.data.id)
     }
--- src/demo/ShoppingCart/ShoppingCart.js
+++ src/demo/ShoppingCart/ShoppingCart.js
@@ -1,17 +1,31 @@
+import $ from 'jquery'
+import getCart from '../ShoppingCart/GetCart'
+
 export default class ShoppingCart{
     constructor(app) {
-
+        this.app = app
+        this.cart = getCart()
+        this.$el = $('<div>').css({
+            'marginBottom': '20px',
+            'border-bottom': '1px solid red'
+        })
     }
     init(){
-
+        this.initBtn()
+        this.render()
     }
     initBtn(){
+        let $btn = $('<button>购物车</button>')
+        $btn.click(() => {
+            this.showCart()
+        })
+        this.$el.append($btn)
 
     }
     showCart(){
-
+        alert(this.cart.getList())
     }
     render(){
-
+        this.app.$el.append(this.$el)
     }
 }
